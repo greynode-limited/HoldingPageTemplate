17671eca738e29dc6de6520e8daab92307244677
James Doel <jamesdoel123@gmail.com>
2021-04-06 22:32:10 +0100

    favicon updates

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ function MyApp({ Component, pageProps }) {
             <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png"></link>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png"></link>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png"></link>
-            <link rel="manifest" href="/site.webmanifest"></link>
+            <link rel="manifest" href="/favicon_io/site.webmanifest"></link>
           </Head>
           <Component {...pageProps} />
          </>
